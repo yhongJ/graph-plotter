f3cfc686cc92aa177d911136816e09b86e943944
yoon <yunhj10211@gmail.com>
2026-08-23 23:51:39 +0900

    feat: start designing front-end (2D coordinate display)

---
+++ src/main/java/kr/co/graph_plotter/GraphPlotterApplication.java
@@ -0,0 +1,13 @@
+package kr.co.graph_plotter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphPlotterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphPlotterApplication.class, args);
+	}
+
+}
---
+++ src/main/java/kr/co/graph_plotter/GraphPlotterController.java
@@ -0,0 +1,10 @@
+package kr.co.graph_plotter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GraphPlotterController {
+
+}
